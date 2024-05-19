7bb4cf1691bd155b7415dadf78c5ca4e21d494e5
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 20:36:42 +0430

    Quiz app

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <h2 dangerouslySetInnerHTML={{__html:question}} />
           <div className="btn-container">
             {correct.map((answer, index)=> {
-              return 
+              return <button dkey={index} className="answer-btn" angerouslySetInnerHTML={{__html:answer}}/>
             })}
           </div>
         </article>
