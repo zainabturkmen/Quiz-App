f7aaac3642d9d55b889c37cb8187acdd183bff23
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 18:52:16 +0430

    Quiz app

--- src/context.js
+++ src/context.js
@@ -38,6 +38,8 @@ const AppProvider = ({ children }) => {
         setLoading(false);
         setWaiting(false)
         setError(false)
+      }else{
+        
       }
     } else {
       setWaiting(true);
