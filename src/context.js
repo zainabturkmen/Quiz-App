3e3d684f8f924f383f3cd39c9da76a2c64b0abb3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 17:28:45 +0430

    Quiz app

--- src/context.js
+++ src/context.js
@@ -26,7 +26,21 @@ const AppProvider = ({ children }) => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return <AppContext.Provider value={{waiting}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
