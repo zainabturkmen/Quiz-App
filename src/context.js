fb2fbe9c4c14d82b2abf178663970aa79210d562
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-19 15:53:48 +0430

    Quiz app

--- src/context.js
+++ src/context.js
@@ -11,6 +11,9 @@ const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
 
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
