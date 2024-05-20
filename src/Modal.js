aead1203e5b744a457037c38d7618fd706375bda
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 10:24:17 +0430

    Quiz app

--- src/Modal.js
+++ src/Modal.js
@@ -11,8 +11,13 @@ const Modal = () => {
     >
       <div className="modal-content">
         <h2>congrats!</h2>
-        <p>You answerd {} of questions correctly</p>
-        <button className="close-btn" onClick={closeModal}>play again</button>
+        <p>
+          You answerd {((correct / questions.length) * 100).toFixed(0)} of
+          questions correctly
+        </p>
+        <button className="close-btn" onClick={closeModal}>
+          play again
+        </button>
       </div>
     </div>
   );
