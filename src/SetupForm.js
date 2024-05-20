2afa892133d514d442baa674552168cfc1b83eba
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 12:54:40 +0430

    Quiz app

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SetupForm = () => {
-  const { quiz, handleChange, handleSubmit,error} = useGlobalContext();
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
   return (
     <main>
       <section className="quiz quiz-small">
         <form className="setup-form">
           <div className="form-control">
             <label htmlFor="amount">number of questions</label>
-            <input type="number" name="amount" id="amount" value={}/>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={quiz.amount}
+              className="form-input"
+              min={1}
+              max={50}
+              onChange={handleChange}
+            />
           </div>
         </form>
       </section>
     </main>
-  ); 
-}
+  );
+};
 
-export default SetupForm
+export default SetupForm;
