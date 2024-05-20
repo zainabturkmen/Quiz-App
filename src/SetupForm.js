7a3fb00cbc26e236520807d6980093fe68d46ba0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 12:55:33 +0430

    Quiz app

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -7,6 +7,7 @@ const SetupForm = () => {
     <main>
       <section className="quiz quiz-small">
         <form className="setup-form">
+          <h2>setup quiz</h2>
           <div className="form-control">
             <label htmlFor="amount">number of questions</label>
             <input
