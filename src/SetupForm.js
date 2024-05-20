5c7817de014aa3ae2f92e5216ac3674efa08ccdf
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 10:44:33 +0430

    Quiz app

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -5,7 +5,9 @@ const SetupForm = () => {
   const { quiz, handleChange, handleSubmit,error} = useGlobalContext();
   return <main>
     <section className='quiz quiz-small'>
-      <form className='setup-form'></form>
+      <form className='setup-form'>
+        <div className='form-control'></div>
+      </form>
     </section>
   </main> 
 }
