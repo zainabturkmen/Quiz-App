9f8a2dafef8a7a9f983fad791c3fba2fc9a38501
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 10:12:14 +0430

    Quiz app

--- src/Modal.js
+++ src/Modal.js
@@ -3,7 +3,13 @@ import { useGlobalContext } from "./context";
 
 const Modal = () => {
   const { isModalOpen, closeModal, correct, questions } = useGlobalContext();
-  return <div className={`${isModalOpen? ""}`}></div>
+  return (
+    <div
+      className={`${
+        isModalOpen ? "modal-container isOpen" : "modal-container"
+      }`}
+    ></div>
+  );
 };
 
 export default Modal;
