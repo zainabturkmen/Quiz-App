aed8dd423bd36af5a747390e2c346ea977fc4c8c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 14:32:44 +0430

    Quiz app

--- src/context.js
+++ src/context.js
@@ -92,6 +92,9 @@ const AppProvider = ({ children }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    const tempUrl =
+      "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
   }
 
 
