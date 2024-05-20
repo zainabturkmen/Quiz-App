be6a33053fd21282750fe2e4d2b5f159f2418fbb
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 14:50:59 +0430

    Quiz app

--- src/context.js
+++ src/context.js
@@ -92,9 +92,6 @@ const AppProvider = ({ children }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const { amount, category, difficulty } = quiz;
-    const tempUrl =
-      "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
-
     const url = `${API_ENDPOINT}amount=${amount}$difficulty={difficulty}&category=${table[category]}&type=multiple`;
   };
 
