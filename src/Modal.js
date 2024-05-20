94eb90072707acae37da29e4e90fe247d2caa943
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-20 10:04:39 +0430

    Quiz app

--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Modal = () => {
-  const {isModalOpen}  = useGlobalContext();
-  return <h2>modal component</h2>
-}
+  const { isModalOpen, closeModal, correct, questions } = useGlobalContext();
+  return <h2>modal component</h2>;
+};
 
-export default Modal
+export default Modal;
